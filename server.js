191236be1c9f6d74fa6a2643212abd2bd9228ab5
Mauro9393 <m.dicarlo@baberlearning.fr>
2025-05-28 16:47:38 +0200

    adding certificate in secret scaleway console

--- server.js
+++ server.js
@@ -50,11 +50,11 @@ app.use(express.json());
 
 // pool DEVE stare prima di qualsiasi route che lo usa
 const pool = new Pool({
-    connectionString: process.env.PG_CONNECTION,
-    ssl: {
-        ca: fs.readFileSync("C:/Users/MauroDiCarlo/rdb-blearn-db.pem").toString(),
-        rejectUnauthorized: true
-    }
+  connectionString: process.env.PG_CONNECTION,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.PG_SSL_CA
+  }
 });
 
 // SSE helper for OpenAI Threads
